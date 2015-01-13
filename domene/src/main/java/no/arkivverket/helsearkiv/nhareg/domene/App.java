d3fa0c3fae7ef5dffe8af50e2f6b78b0a0ffd3cc
arnfinns <arnfinns@192.168.10.103>
2015-01-13 20:38:22 +0100

    NHA-3 Lagt til xjb-bindings og avhengighet til validering.

--- domene/src/main/java/no/arkivverket/helsearkiv/nhareg/domene/App.java
+++ domene/src/main/java/no/arkivverket/helsearkiv/nhareg/domene/App.java
@@ -1,11 +1,15 @@
 package no.arkivverket.helsearkiv.nhareg.domene;
 
+import javax.validation.constraints.NotNull;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+    @NotNull
+    int i;
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
