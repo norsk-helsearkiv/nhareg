64e9778775f6f43046539ea040312cd6621f5ebc
arnfinns <arnfinns@192.168.10.113>
2015-01-23 10:21:57 +0100

    Revert "Dette skjemaet skal tilsvare en avlevering og ikke en avtale. Derfor må rot-element være avlevering."
    This reverts commit b4d2e6bf70f88202c995de357b62e5e9ec2e735e.

--- tjeneste/src/main/java/no/arkivverket/helsearkiv/nhareg/tjeneste/AvleveringTjeneste.java
+++ tjeneste/src/main/java/no/arkivverket/helsearkiv/nhareg/tjeneste/AvleveringTjeneste.java
@@ -1,7 +1,6 @@
 package no.arkivverket.helsearkiv.nhareg.tjeneste;
 
 import javax.ejb.Stateless;
-import javax.validation.Validator;
 import javax.ws.rs.Path;
 import no.arkivverket.helsearkiv.nhareg.domene.avlevering.Avlevering;
 
@@ -13,18 +12,10 @@ import no.arkivverket.helsearkiv.nhareg.domene.avlevering.Avlevering;
  *
  */
 @Path("/avleveringer")
-/**
- * <p>
- * Dette er en stateless service, vi deklarer den som EJB for å få
- * transaksjonsstøtte.
- * </p>
- */
 @Stateless
 public class AvleveringTjeneste extends EntitetsTjeneste<Avlevering, String> {
 
     public AvleveringTjeneste() {
         super(Avlevering.class, String.class, "avleveringsidentifikator");
-
     }
-
 }
--- tjeneste/src/main/java/no/arkivverket/helsearkiv/nhareg/tjeneste/AvtaleTjeneste.java
+++ tjeneste/src/main/java/no/arkivverket/helsearkiv/nhareg/tjeneste/AvtaleTjeneste.java
@@ -1,7 +1,6 @@
 package no.arkivverket.helsearkiv.nhareg.tjeneste;
 
 import javax.ejb.Stateless;
-import javax.validation.Validator;
 import javax.ws.rs.Path;
 import no.arkivverket.helsearkiv.nhareg.domene.avlevering.Avtale;
 
@@ -13,18 +12,11 @@ import no.arkivverket.helsearkiv.nhareg.domene.avlevering.Avtale;
  *
  */
 @Path("/avtaler")
-/**
- * <p>
- * Dette er en stateless service, vi deklarer den som EJB for å få
- * transaksjonsstøtte.
- * </p>
- */
 @Stateless
 public class AvtaleTjeneste extends EntitetsTjeneste<Avtale, String> {
 
     public AvtaleTjeneste() {
         super(Avtale.class, String.class, "avtaleidentifikator");
-
-    }
-
+    
+        }
 }
