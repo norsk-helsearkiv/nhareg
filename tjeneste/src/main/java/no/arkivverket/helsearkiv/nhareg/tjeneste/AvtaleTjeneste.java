4d9ef9827e22f4c11cb70a92184ee87a8a17adf9
Robin Gulbrandsen <robin@123.no>
2015-01-28 13:12:58 +0100

    Lagt til oppdateringsmetode for avtale

--- tjeneste/src/main/java/no/arkivverket/helsearkiv/nhareg/tjeneste/AvtaleTjeneste.java
+++ tjeneste/src/main/java/no/arkivverket/helsearkiv/nhareg/tjeneste/AvtaleTjeneste.java
@@ -5,6 +5,8 @@ import javax.ejb.Stateless;
 import javax.persistence.Query;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import static javax.ws.rs.HttpMethod.PUT;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -45,6 +47,22 @@ public class AvtaleTjeneste extends EntitetsTjeneste<Avtale, String> {
         query.setParameter("avtaleidentifikator", avtaleidentifikator);
         List<Avlevering> avleveringer = query.getResultList();
         return avleveringer;
+    }
+    
+    @PUT
+    @Path("/oppdater")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response oppdaterAvtale(Avtale avtale) {
+        if(avtale == null) {
+            return Response.noContent().build();
+        }
+        Avtale persistertAvtale = super.getEntityManager().find(Avtale.class, avtale.getAvtaleidentifikator());
+        if(persistertAvtale == null) {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+        
+        super.getEntityManager().merge(avtale);
+        return Response.ok(avtale).build();  
     }
     
     @DELETE
