ecde0ebc20e0b01d2dada13713ef4679146d344d
Robin Gulbrandsen <robin@123.no>
2015-01-28 10:40:01 +0100

    NHA-53 Paging for pasientjournaler

--- tjeneste/src/main/java/no/arkivverket/helsearkiv/nhareg/tjeneste/AvleveringTjeneste.java
+++ tjeneste/src/main/java/no/arkivverket/helsearkiv/nhareg/tjeneste/AvleveringTjeneste.java
@@ -1,14 +1,25 @@
 package no.arkivverket.helsearkiv.nhareg.tjeneste;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Formatter;
 import java.util.List;
 import javax.ejb.Stateless;
+import javax.persistence.Query;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.ResponseBuilder;
+import javax.ws.rs.core.UriInfo;
 import no.arkivverket.helsearkiv.nhareg.domene.avlevering.Avlevering;
 import no.arkivverket.helsearkiv.nhareg.domene.avlevering.Pasientjournal;
+import no.arkivverket.helsearkiv.nhareg.domene.avlevering.wrapper.ListeObjekt;
 
 /**
  * <p>
@@ -34,9 +45,59 @@ public class AvleveringTjeneste extends EntitetsTjeneste<Avlevering, String> {
     @GET
     @Path("/{id}/pasientjournaler")
     @Produces(MediaType.APPLICATION_JSON)
-    public List<Pasientjournal> getPasientjournaler(@PathParam("id") String avleveringsidentifikator) {
-        List<Pasientjournal> pasientjournaler = super.getSingleInstance(avleveringsidentifikator).getPasientjournal();
-        return pasientjournaler;
+    public ListeObjekt getPasientjournaler(@Context UriInfo uriInfo, @PathParam("id") String avleveringsidentifikator) {
+        //Hent total count
+        String totalJpql = "SELECT a FROM Avlevering a WHERE a.avleveringsidentifikator LIKE :id";
+        Query totalQuery = super.getEntityManager().createQuery(totalJpql);
+        totalQuery.setParameter("id", avleveringsidentifikator);
+        Avlevering avlevering = (Avlevering) totalQuery.getSingleResult();
+        
+        //Hent url parameter
+        MultivaluedMap<String, String> queryParameters = uriInfo.getPathParameters();
+        int total = avlevering.getPasientjournal().size();
+        int forste = 0;
+        int side = 1;
+        int antall = total;
+        if (queryParameters.containsKey("side") && queryParameters.containsKey("antall")) {
+            int paramSide = Integer.parseInt(queryParameters.getFirst("side")) - 1;
+            int paramAntall = Integer.parseInt(queryParameters.getFirst("antall"));
+            
+            if(paramSide > 0 && paramAntall > 0) {
+                side = paramSide;
+                antall = paramAntall;
+                
+                forste = (side * antall) -1;
+            }
+        }
+        
+        List<Pasientjournal> pasientjournaler = new ArrayList<Pasientjournal>();
+        for(int i = forste; i < antall; i++) {
+            pasientjournaler.add(avlevering.getPasientjournal().get(i));
+        }
+        
+        //Returner objekt
+        return new ListeObjekt(pasientjournaler, total, side, antall);
+    }
+    
+    
+    @GET
+    @Path("/{id}/leveranse")
+    @Produces(MediaType.APPLICATION_OCTET_STREAM)
+    public Response getLeveranse(@PathParam("id") String avleveringsidentifikator) throws FileNotFoundException {
+        //Tmp fil
+        File file = new File("leveranse");
+        Formatter out = new Formatter(file);
+        out.format("data");
+        out.close();
+        
+        //Generer XML
+        
+        //Legg til vedlegg
+        
+        //Returner fil
+        ResponseBuilder response = Response.ok((Object) file);
+        response.header("Content-Disposition", "attachment; filename=" + avleveringsidentifikator + ".zip");
+        return response.build();
     }
 
 }
