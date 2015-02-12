85dcac5e744df1679cf4ea28713b02f7667a5dc2
arnfinns <arnfinns@192.168.10.119>
2015-02-12 19:48:05 +0100

    NHA-67 Filtrering på søketerm.

---
+++ tjeneste/src/main/java/no/arkivverket/helsearkiv/nhareg/util/PasientjournalSokestringPredicate.java
@@ -0,0 +1,49 @@
+package no.arkivverket.helsearkiv.nhareg.util;
+
+//import com.sun.xml.internal.ws.util.StringUtils;
+import java.util.List;
+import no.arkivverket.helsearkiv.nhareg.domene.avlevering.Grunnopplysninger;
+import no.arkivverket.helsearkiv.nhareg.domene.avlevering.Identifikator;
+import no.arkivverket.helsearkiv.nhareg.domene.avlevering.Pasientjournal;
+import org.apache.commons.collections4.Predicate;
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ *
+ * @author arnfinns
+ */
+public class PasientjournalSokestringPredicate implements Predicate<Pasientjournal> {
+
+    List<String> sokestrings;
+
+    public PasientjournalSokestringPredicate(List<String> sokestrings) {
+        this.sokestrings = sokestrings;
+    }
+
+    public boolean evaluate(Pasientjournal pasientjournal) {
+        boolean evaluate = false;
+        if (pasientjournal.getGrunnopplysninger() != null) {
+            Grunnopplysninger grunnopplysninger = pasientjournal.getGrunnopplysninger();
+            if (grunnopplysninger.getPnavn() != null) {
+                String pnavn = grunnopplysninger.getPnavn();
+                for (String sokestring : sokestrings) {
+                    if (StringUtils.containsIgnoreCase(pnavn, sokestring)) {
+                        evaluate = true;
+                    }
+                }
+            }
+            if (!evaluate) {
+                if (grunnopplysninger.getIdentifikator() != null) {
+                    Identifikator identifikator = grunnopplysninger.getIdentifikator();
+                    String pid = identifikator.getPID();
+                    for (String sokestring : sokestrings) {
+                        if (StringUtils.containsIgnoreCase(pid, sokestring)) {
+                            evaluate = true;
+                        }
+                    }
+                }
+            }
+        }
+        return evaluate;
+    }
+}
