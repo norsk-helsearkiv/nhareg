ac9e67adef563713a0b7cbc602d515675d5e9e93
haraldk <harald.ak@gmail.com>
2015-04-24 13:11:59 +0200

    NHA-134 entiteter for sikkerhet.

---
+++ domene/src/main/java/no/arkivverket/helsearkiv/nhareg/domene/auth/Bruker.java
@@ -0,0 +1,50 @@
+package no.arkivverket.helsearkiv.nhareg.domene.auth;
+
+import javax.persistence.*;
+
+/**
+ * Created by haraldk on 15.04.15.
+ */
+@Entity
+public class Bruker {
+    @Id
+    private String brukernavn;
+    private String passord;
+    private String authToken;
+
+    @ManyToOne(fetch=FetchType.LAZY)
+    @JoinColumn(name="rollenavn")
+    private Rolle rolle;
+
+    public String getBrukernavn() {
+        return brukernavn;
+    }
+
+    public void setBrukernavn(String brukernavn) {
+        this.brukernavn = brukernavn;
+    }
+
+    public String getPassord() {
+        return passord;
+    }
+
+    public void setPassord(String passord) {
+        this.passord = passord;
+    }
+
+    public Rolle getRolle() {
+        return rolle;
+    }
+
+    public void setRolle(Rolle rolle) {
+        this.rolle = rolle;
+    }
+    public String getAuthToken() {
+        return authToken;
+    }
+
+    public void setAuthToken(String authToken) {
+        this.authToken = authToken;
+    }
+
+}
---
+++ domene/src/main/java/no/arkivverket/helsearkiv/nhareg/domene/auth/Rolle.java
@@ -0,0 +1,24 @@
+package no.arkivverket.helsearkiv.nhareg.domene.auth;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+/**
+ * Created by haraldk on 15.04.15.
+ */
+
+
+@Entity
+public class Rolle {
+
+    @Id
+    private String navn;
+
+    public String getNavn() {
+        return navn;
+    }
+
+    public void setNavn(String navn) {
+        this.navn = navn;
+    }
+}
