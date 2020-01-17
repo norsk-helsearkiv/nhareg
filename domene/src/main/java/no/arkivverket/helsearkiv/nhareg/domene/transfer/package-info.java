9216fc54d98aa7719d6c967ee39cb77a753c14d4
Jonas Sandbekk <jonass@netcompany.com>
2020-01-17 15:42:38 +0100

    NHA-185 Updated package-info for domene.transfer.
    https://ncdev.atlassian.net/browse/NHA-185

--- domene/src/main/java/no/arkivverket/helsearkiv/nhareg/domene/transfer/package-info.java
+++ domene/src/main/java/no/arkivverket/helsearkiv/nhareg/domene/transfer/package-info.java
@@ -2,21 +2,30 @@
 * Schema information for generating XML.
  **/
 
-@javax.xml.bind.annotation.XmlSchema(
+@XmlSchema(
     namespace = "http://www.arkivverket.no/standarder/nha/avlxml",
     elementFormDefault = XmlNsForm.QUALIFIED,
-    xmlns = @XmlNs(prefix = "", namespaceURI = "http://www.arkivverket.no/standarder/nha/avlxml")
+    xmlns = {
+        @XmlNs(prefix = "", namespaceURI = "http://www.arkivverket.no/standarder/nha/avlxml")
+    }
 )
 @XmlJavaTypeAdapters({    
     @XmlJavaTypeAdapter(value = StringAdapter.class, type = String.class),
-    @XmlJavaTypeAdapter(value = DateOrYearAdapter.class, type = DateOrYear.class)
+    @XmlJavaTypeAdapter(value = DateOrYearAdapter.class, type = DateOrYear.class),
+    @XmlJavaTypeAdapter(value = LocalDateTimeAdapter.class, type = LocalDateTime.class),
+    @XmlJavaTypeAdapter(value = LocalDateAdapter.class, type = LocalDate.class)
 })
 package no.arkivverket.helsearkiv.nhareg.domene.transfer;
 
 import no.arkivverket.helsearkiv.nhareg.domene.adapter.DateOrYearAdapter;
+import no.arkivverket.helsearkiv.nhareg.domene.adapter.LocalDateAdapter;
+import no.arkivverket.helsearkiv.nhareg.domene.adapter.LocalDateTimeAdapter;
 import no.arkivverket.helsearkiv.nhareg.domene.adapter.StringAdapter;
 
 import javax.xml.bind.annotation.XmlNs;
 import javax.xml.bind.annotation.XmlNsForm;
+import javax.xml.bind.annotation.XmlSchema;
 import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
 import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapters;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
