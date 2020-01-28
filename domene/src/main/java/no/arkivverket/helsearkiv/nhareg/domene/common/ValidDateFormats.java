83a211f5a6b48c5c5f8c73182c75c13d3506e649
Jonas Sandbekk <jonass@netcompany.com>
2020-01-28 10:37:52 +0100

    NHA-185 - Fixed ValidDateFormats not recognizing uuuu-MM-dd formats.
    https://ncdev.atlassian.net/browse/NHA-185

--- domene/src/main/java/no/arkivverket/helsearkiv/nhareg/domene/common/ValidDateFormats.java
+++ domene/src/main/java/no/arkivverket/helsearkiv/nhareg/domene/common/ValidDateFormats.java
@@ -8,33 +8,35 @@ import java.time.format.ResolverStyle;
 import java.time.temporal.ChronoField;
 
 public class ValidDateFormats {
-    
-    private static final String PATTERNS = 
+
+    private static final String PATTERNS =
         "[d.M.uuuu]"
-        + "[dd.MM.uuuu]"
-        + "[d.MM.uuuu]"
-        + "[dd.M.uuuu]"
-        + "[d,M,uuuu]"
-        + "[dd,MM,uuuu]"
-        + "[d,MM,uuuu]"
-        + "[dd,M,uuuu]"
-        + "[d-M-uuuu]"
-        + "[dd-MM-uuuu]"
-        + "[d-MM-uuuu]"
-        + "[dd-M-uuuu]"
-        + "[ddMMuuuu]" 
-        + "[uuuu]";
+            + "[uuuu-MM-dd]"
+            + "[dd.MM.uuuu]"
+            + "[d.MM.uuuu]"
+            + "[dd.M.uuuu]"
+            + "[d,M,uuuu]"
+            + "[dd,MM,uuuu]"
+            + "[d,MM,uuuu]"
+            + "[dd,M,uuuu]"
+            + "[d-M-uuuu]"
+            + "[dd-MM-uuuu]"
+            + "[d-MM-uuuu]"
+            + "[dd-M-uuuu]"
+            + "[ddMMuuuu]"
+            + "[uuuu]";
 
     public static LocalDate getDate(final String date) {
         final DateTimeFormatterBuilder formatterBuilder = new DateTimeFormatterBuilder().parseCaseInsensitive();
 
-        formatterBuilder.appendPattern(PATTERNS)
-                        .parseDefaulting(ChronoField.MONTH_OF_YEAR, 1)
-                        .parseDefaulting(ChronoField.DAY_OF_MONTH, 1);
+        final DateTimeFormatter formatter = formatterBuilder.appendPattern(PATTERNS)
+                                                            .parseDefaulting(ChronoField.MONTH_OF_YEAR, 1)
+                                                            .parseDefaulting(ChronoField.DAY_OF_MONTH, 1)
+                                                            .toFormatter()
+                                                            .withResolverStyle(ResolverStyle.STRICT);
 
         try {
-            final DateTimeFormatter formatter = formatterBuilder.toFormatter().withResolverStyle(ResolverStyle.STRICT);
-            return LocalDate.parse(date, formatter);
+            return formatter.parse(date, LocalDate::from);
         } catch (DateTimeParseException ignored) {
             return null;
         }
