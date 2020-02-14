b13806c0b15c5627c7b1bfd57ed43fa463c89508
Jonas <jonass@netcompany.com>
2020-02-14 16:53:41 +0100

    NHA-208 - Made printer field persist when updating users.
    https://ncdev.atlassian.net/browse/NHA-208

--- klient-web/src/main/src/app/home/user-admin/user-admin-controller.js
+++ klient-web/src/main/src/app/home/user-admin/user-admin-controller.js
@@ -1,77 +1,75 @@
 angular.module('nha.home')
 
-    .controller('UserAdminCtrl', function($rootScope, $scope, $location, $filter, httpService, errorService){
-        $scope.bruker = {};
-        $scope.bruker.printerzpl = '127.0.0.1';
-        $scope.brukere = [];
+  .controller('UserAdminCtrl', function($rootScope, $scope, $location, $filter, httpService, errorService){
+    $scope.bruker = {};
+    $scope.bruker.printerzpl = '127.0.0.1';
+    $scope.brukere = [];
+    $scope.selectedBrukerRow = null;
+    $scope.error = [];
+
+    $scope.selectUser = function (selectedUser, index) {
+      if (index === $scope.selectedBrukerRow) {
         $scope.selectedBrukerRow = null;
-        $scope.error = [];
+        $scope.bruker.brukernavn = null;
+        $scope.bruker.rolle = null;
+        $scope.bruker.printerzpl = null;
+      } else {
+        var rolleIndex = $scope.roller.map(function (e) {
+          return e.navn;
+        }).indexOf(selectedUser.rolle.navn);
+        $scope.selectedBrukerRow = index;
+        $scope.bruker.brukernavn = selectedUser.brukernavn;
+        $scope.bruker.printerzpl = selectedUser.printerzpl;
+        $scope.bruker.rolle = $scope.roller[rolleIndex];
+      }
+    };
 
-        $scope.selectUser = function (selectedUser, index) {
-            if (index === $scope.selectedBrukerRow) {
-                $scope.selectedBrukerRow = null;
-                $scope.bruker.brukernavn = null;
-                $scope.bruker.rolle = null;
-                $scope.bruker.printerzpl = null;
-            } else {
-                var rolleIndex = $scope.roller.map(function (e) {
-                    return e.navn;
-                }).indexOf(selectedUser.rolle.navn);
-                $scope.selectedBrukerRow = index;
-                $scope.bruker.brukernavn = selectedUser.brukernavn;
-                $scope.bruker.printerzpl = selectedUser.printerzpl;
-                $scope.bruker.rolle = $scope.roller[rolleIndex];
-            }
-        };
+    $scope.getUsers = function () {
+      httpService.getAll("admin/brukere", false)
+        .success(function (data) {
+          $scope.brukere = data;
+        }).error(function (data, status) {
+        errorService.errorCode(status);
+      });
+    };
 
-        $scope.getUsers = function () {
-            httpService.getAll("admin/brukere", false)
-                .success(function (data) {
-                    $scope.brukere = data;
-                }).error(function (data, status) {
-                    errorService.errorCode(status);
-                });
-        };
+    var resetUser = function () {
+      $scope.bruker = {'printerpzl': $scope.bruker.printerpzl };
+    };
 
-        var resetUser = function () {
-            $scope.bruker = {};
-        };
+    var arePasswordsIdentical = function () {
+      return $scope.bruker.password === $scope.bruker.passwordConfirm;
+    };
 
-        var arePasswordsIdentical = function () {
-            return $scope.bruker.password === $scope.bruker.passwordConfirm;
-        };
+    $scope.updateUser = function () {
+      $scope.error = [];
 
-        $scope.updateUser = function () {
-            $scope.error = [];
+      if (!arePasswordsIdentical()) {
+        $scope.error['passord'] = $filter('translate')('formError.NotIdentical');
+        return;
+      }
 
-            if (!arePasswordsIdentical()) {
-                $scope.error['passord'] = $filter('translate')('formError.NotIdentical');
-                return;
-            }
+      httpService.create("admin/brukere", $scope.bruker)
+        .success(function () {
+          $scope.getUsers();
+          resetUser();
+        })
+        .error(function (data, status) {
+          if (status !== 400) {
+            errorService.errorCode(status);
+          } else {
+            setErrorMessages(data);
+          }
+        });
+    };
 
-            httpService.create("admin/brukere", $scope.bruker)
-                .success(function () {
-                    $scope.getUsers();
-                    resetUser();
-                })
-                .error(function (data, status) {
-                    if (status != 400) {
-                        errorService.errorCode(status);
-                        return;
-                    } else {
-                        setErrorMessages(data);
-                    }
-                });
-        };
+    $scope.showError = function (attribute) {
+      return $scope.error[attribute] !== undefined;
+    };
 
-        $scope.showError = function (attribute) {
-            var error = $scope.error[attribute] !== undefined;
-            return error;
-        };
-
-        var setErrorMessages = function (data) {
-            angular.forEach(data, function (element) {
-                $scope.error[element.attribute] = $filter('translate')('formError.' + element.constraint);
-            });
-        };
-    });
+    var setErrorMessages = function (data) {
+      angular.forEach(data, function (element) {
+        $scope.error[element.attribute] = $filter('translate')('formError.' + element.constraint);
+      });
+    };
+  });
